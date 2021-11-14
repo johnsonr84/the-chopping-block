3f4749990794fc27aa8b33c26f5602cb9850910f
Your Name <robertcjohnson1984@gmail.com>
2021-11-14 15:48:38 -0700

    update with Router and rm StorePicker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/StorePicker';
+import Router from "./components/Router";
 import './css/style.css';
 
-render(<StorePicker />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
