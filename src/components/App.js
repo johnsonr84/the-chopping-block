7da6e65e809169cab6185fd57e366376e475ef6b
Your Name <robertcjohnson1984@gmail.com>
2021-11-14 16:41:38 -0700

    add set stat and add function

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,26 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    };
+    addFish = fish => {
+        // 1. Take a copy of the existing state
+        const fishes = { ...this.state.fishes };
+        // 2. Add our new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        // 3. Set the new fishes object to state
+        this.setState({ fishes });
+    };
     render() {
         return (
-            <div className="react-restaurant-menu">
+            <div className="react-market-inventory">
                 <div className="menu">
-                    <Header tagline="Fresh Seafood Market" age={100} />
+                    <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} />
             </div>
         );
     }
