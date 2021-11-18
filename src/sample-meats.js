cc7e1c292d30f8e2b31bb438743c57cb952be016
Your Name <robertcjohnson1984@gmail.com>
2021-11-18 14:03:57 -0700

    init commit on file

---
+++ src/sample-meats.js
@@ -0,0 +1,84 @@
+// This is just some sample data so you don't have to think of your own!
+const meats = {
+  meat1: {
+    name: "Chicken",
+    image: "/images/chicken.jpg",
+    desc:
+      "Everyone’s favorite white meat. Chicken is the most common type of poultry in the world.",
+    price: 829,
+    status: "available"
+  },
+
+  meat2: {
+    name: "Filet Mignon",
+    image: "/images/filet.jpg",
+    desc:
+      "These tender, mouth-watering beauties are a fantastic hit at any fancy dinner party.",
+    price: 1900,
+    status: "available"
+  },
+
+  meat3: {
+    name: "Ground Beef",
+    image: "/images/hamburger.jpg",
+    desc:
+      "Lean ground beef is a versatile protien. It's healthy in moderation and can be incorporated in many types of recipies",
+    price: 1684,
+    status: "unavailable"
+  },
+
+  meat4: {
+    name: "Bison",
+    image: "/images/bison.jpg",
+    desc:
+      "Take a walk on the wild side with this lean, moderately firm texture with a robust flavor profile red meat. ",
+    price: 2129,
+    status: "available"
+  },
+
+  meat5: {
+    name: "Pork Chops",
+    image: "/images/porkchops.jpg",
+    desc:
+      "Fantastic loin cut meat perfect for grilling. Pair with potatoes, your favorite veggies and a side of apple sauce!",
+    price: 1334,
+    status: "available"
+  },
+
+  meat6: {
+    name: "Bacon",
+    image: "/images/bacon.jpg",
+    desc:
+      "Thick sliced salt cured bacon that's full of flavor. A perfect accent to any dish...it's as good as it gets!",
+    price: 1053,
+    status: "available"
+  },
+
+  meat7: {
+    name: "Ribs",
+    image: "/images/ribs.jpg",
+    desc: "These flavor packet ribs are a game day hit. Packed with tons of flavor, add your favorite sauce and cook slowly with moist heat.",
+    price: 1643,
+    status: "available"
+  },
+
+  meat8: {
+    name: "Meatballs",
+    image: "/images/meatballs.jpg",
+    desc:
+      "The best handmade meatballs with a fresh taste of Italy packed with a full-flavored and complex taste.",
+    price: 1025,
+    status: "available"
+  },
+
+  meat9: {
+    name: "Tomahawks",
+    image: "/images/tomahawks.jpg",
+    desc:
+      "Endulge with these delectable tomahawk steaks, featuring extra long French style trimmed bone.",
+    price: 1950,
+    status: "available"
+  }
+};
+
+export default meats;
