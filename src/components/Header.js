8707df8c7ebec2e7ebc6a9426e68fbbb205df966
Your Name <robertcjohnson1984@gmail.com>
2021-11-18 22:09:59 -0700

    add proptypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Header = props => (
     <header className="top">
@@ -14,5 +15,9 @@ const Header = props => (
         </h3>
     </header>
 );
+
+Header.propTypes = {
+    tagline: PropTypes.string.isRequired
+};
 
 export default Header;
