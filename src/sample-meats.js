f8f64f72ffebabde10e0e4e0f77c34eb4ef0a1c2
Your Name <robertcjohnson1984@gmail.com>
2021-11-19 10:40:51 -0700

    update img files

--- src/sample-meats.js
+++ src/sample-meats.js
@@ -11,7 +11,7 @@ const meats = {
 
   meat2: {
     name: "Filet Mignon",
-    image: "/images/filet.jpg",
+    image: "/images/filet.png",
     desc:
       "These tender, mouth-watering beauties are a fantastic hit at any fancy dinner party.",
     price: 1900,
@@ -20,7 +20,7 @@ const meats = {
 
   meat3: {
     name: "Ground Beef",
-    image: "/images/hamburger.jpg",
+    image: "/images/hamburger.png",
     desc:
       "Lean ground beef is a versatile protien. It's healthy in moderation and can be incorporated in many types of recipies",
     price: 1684,
@@ -28,52 +28,52 @@ const meats = {
   },
 
   meat4: {
-    name: "Bison",
-    image: "/images/bison.jpg",
-    desc:
-      "Take a walk on the wild side with this lean, moderately firm texture with a robust flavor profile red meat. ",
-    price: 2129,
-    status: "available"
-  },
-
-  meat5: {
     name: "Pork Chops",
-    image: "/images/porkchops.jpg",
+    image: "/images/porkchops.png",
     desc:
       "Fantastic loin cut meat perfect for grilling. Pair with potatoes, your favorite veggies and a side of apple sauce!",
     price: 1334,
     status: "available"
   },
 
-  meat6: {
+  meat5: {
     name: "Bacon",
-    image: "/images/bacon.jpg",
+    image: "/images/bacon.png",
     desc:
       "Thick sliced salt cured bacon that's full of flavor. A perfect accent to any dish...it's as good as it gets!",
     price: 1053,
     status: "available"
   },
 
-  meat7: {
-    name: "Ribs",
-    image: "/images/ribs.jpg",
-    desc: "These flavor packet ribs are a game day hit. Packed with tons of flavor, add your favorite sauce and cook slowly with moist heat.",
-    price: 1643,
-    status: "available"
-  },
-
-  meat8: {
+  meat6: {
     name: "Meatballs",
-    image: "/images/meatballs.jpg",
+    image: "/images/meatballs.png",
     desc:
       "The best handmade meatballs with a fresh taste of Italy packed with a full-flavored and complex taste.",
     price: 1025,
     status: "available"
   },
 
+  meat7: {
+    name: "Ribs",
+    image: "/images/ribs.png",
+    desc: "These flavor packet ribs are a game day hit. Packed with tons of flavor, add your favorite sauce and cook slowly with moist heat.",
+    price: 1643,
+    status: "available"
+  },
+
+  meat8: {
+    name: "Bison",
+    image: "/images/bison.png",
+    desc:
+      "Take a walk on the wild side with this lean, moderately firm texture with a robust flavor profile red meat. ",
+    price: 2129,
+    status: "available"
+  },
+
   meat9: {
     name: "Tomahawks",
-    image: "/images/tomahawks.jpg",
+    image: "/images/tomahawks.png",
     desc:
       "Endulge with these delectable tomahawk steaks, featuring extra long French style trimmed bone.",
     price: 1950,
