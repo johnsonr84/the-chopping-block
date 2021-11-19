b2f18ed06f8b6878a341b5ae27592cc32d1bdffb
Your Name <robertcjohnson1984@gmail.com>
2021-11-18 22:11:52 -0700

    add static proptypes

--- src/components/Meat.js
+++ src/components/Meat.js
@@ -1,7 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { formatPrice } from "../helpers";
 
 class Meat extends React.Component {
+    static propTypes = {
+        details: PropTypes.shape({
+            image: PropTypes.string,
+            name: PropTypes.string,
+            desc: PropTypes.string,
+            status: PropTypes.string,
+            price: PropTypes.number
+        }),
+        addToOrder: PropTypes.func,
+        index: PropTypes.string
+    };
     render() {
         const { image, name, price, desc, status } = this.props.details;
         const isAvailable = status === "available";
